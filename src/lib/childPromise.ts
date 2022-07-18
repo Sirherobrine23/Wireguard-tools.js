2d8a3db8277f47f9f398a5821ccb752eb1c1a218
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-07-18 03:30:10 -0300

    Squashed commit of the following:
    commit aaa2ef034cb2466cf9a18977c649e5ee2130edc6
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Fri Jul 15 18:11:55 2022 +0000
    
        Stash
    
    commit 41df6971cf7cc42139b9c780a6272bf4538ae082
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Thu Jul 14 14:29:13 2022 +0000
    
        Init netlink
    
        Currently netlink is not getting the device, I'm still testing the library.
    
    commit 4dee93c7829da1a8e7b45854da9a55b93e5dfb67
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Thu Jul 14 12:27:08 2022 +0000
    
        Update vscode devcontainer and github actions
    
    commit 0367b27e73476af8205ecbc36957866eaa1342c8
    Merge: 1848948 33ae6e1
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Thu Jul 14 00:02:33 2022 -0300
    
        Merge branch 'main' into netlink
    
    commit 18489483d78b9aa5441cc122f4df8943d45b156f
    Merge: 4fbbcfa 1a8c15b
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Wed Jul 13 23:40:18 2022 -0300
    
        Merge branch 'main' into netlink
    
    commit 4fbbcfa9bb3ed7a3e3372e85e208a161584a3758
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Wed Jul 13 23:39:46 2022 -0300
    
        Clean repository folders
    
    commit 2f136b5b767b3a50f58d44b3afdabb75ddb9dbd1
    Author: Matheus Sampaio Queiroga <srherobrine20@gmail.com>
    Date:   Wed Jul 13 22:56:52 2022 +0000
    
        Update base

--- src/lib/childPromise.ts
+++ src/lib/childPromise.ts
@@ -1,12 +1,28 @@
 import * as child_process from "node:child_process";
 
-export default async function run(command, args: string[], env: {[ke: string]: string}, cwd?: string): Promise<{stderr: string, stdout: string}> {
+export default run;
+export async function run(command, args: string[], env: {[ke: string]: string}, cwd?: string): Promise<{stderr: string, stdout: string}> {
   return new Promise((res, rej) => {
     child_process.execFile(command, args, {
       cwd: cwd,
       env: {
         ...process.env,
         ...env
+      }
+    }, (err, stdout, stderr) => {
+      if (!!err) return rej(err);
+      return res({stderr, stdout});
+    });
+  });
+}
+
+export async function singleRun(command: string, env?: {[ke: string]: string}, cwd?: string): Promise<{stderr: string, stdout: string}> {
+  return new Promise((res, rej) => {
+    child_process.exec(command, {
+      cwd: cwd,
+      env: {
+        ...process.env,
+        ...(env||{})
       }
     }, (err, stdout, stderr) => {
       if (!!err) return rej(err);
