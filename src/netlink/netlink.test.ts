dab7b3ce3b5f44208c4572dacd26633e5e179721
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-07-20 15:07:19 +0000

    Update netlink test

--- src/netlink/netlink.test.ts
+++ src/netlink/netlink.test.ts
@@ -1,5 +1,20 @@
 import * as wireguardNetlink from "./index";
 
 export default async function main() {
-  const getDevices = await wireguardNetlink.getDevices();
+  try {
+    await wireguardNetlink.getDevices();
+  } catch(err) {
+    console.log("Get devices works:", err);
+  }
+
+  // Create interface and attempt to get devices again
+  await wireguardNetlink.addInterface({
+    interfaceName: "wg-test",
+    ip: ["10.0.0.1/24"]
+  });
+  const wireguardDevices = await wireguardNetlink.getDevices();
+  if (!wireguardDevices.some(x => x.name === "wg-test")) throw new Error("Interface not found");
+  console.log("Interface created");
+
+  return console.log(wireguardDevices);
 }
