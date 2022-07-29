0e1e65a057692d8f70f1c1d0bd1418adf4852cd4
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-07-29 01:25:50 +0000

    Add peer endpoint

--- addon/binding.cc
+++ addon/binding.cc
@@ -3,6 +3,13 @@
 #include <string>
 #include <napi.h>
 
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netdb.h>
+
 // Wireguard embedded library.
 #ifdef __cplusplus
 extern "C" {
@@ -51,14 +58,37 @@ Napi::Object getPeers(const Napi::CallbackInfo& info) {
       // Keep interval alive
       if (peer->persistent_keepalive_interval) PeerObj.Set(Napi::String::New(info.Env(), "keepInterval"), Napi::Number::New(info.Env(), peer->persistent_keepalive_interval));
 
+      if (peer->endpoint.addr.sa_family == AF_INET||peer->endpoint.addr.sa_family == AF_INET6) {
+        const struct sockaddr *addr = &peer->endpoint.addr;
+        char host[4096 + 1];
+        char service[512 + 1];
+        static char buf[sizeof(host) + sizeof(service) + 4];
+        int ret;
+        socklen_t addr_len = 0;
+
+        memset(buf, 0, sizeof(buf));
+        if (addr->sa_family == AF_INET)
+          addr_len = sizeof(struct sockaddr_in);
+        else if (addr->sa_family == AF_INET6)
+          addr_len = sizeof(struct sockaddr_in6);
+
+        ret = getnameinfo(addr, addr_len, host, sizeof(host), service, sizeof(service), NI_DGRAM | NI_NUMERICSERV | NI_NUMERICHOST);
+        if (ret) {
+          strncpy(buf, gai_strerror(ret), sizeof(buf) - 1);
+          buf[sizeof(buf) - 1] = '\0';
+        } else
+          snprintf(buf, sizeof(buf), (addr->sa_family == AF_INET6 && strchr(host, ':')) ? "[%s]:%s" : "%s:%s", host, service);
+        PeerObj.Set(Napi::String::New(info.Env(), "endpoint"), Napi::String::New(info.Env(), buf));
+      }
+
       // Bytes sent and received
       if (peer->rx_bytes) PeerObj.Set(Napi::String::New(info.Env(), "rxBytes"), Napi::Number::New(info.Env(), peer->rx_bytes));
       if (peer->tx_bytes) PeerObj.Set(Napi::String::New(info.Env(), "txBytes"), Napi::Number::New(info.Env(), peer->tx_bytes));
 
       // Latest handshake
       if (peer->last_handshake_time.tv_sec) {
-        time_t now = time(NULL);
-        double lastHandshake = peer->last_handshake_time.tv_sec - now;
+        // time_t now = time(NULL);
+        double lastHandshake = peer->last_handshake_time.tv_sec * 1000;
         PeerObj.Set(Napi::String::New(info.Env(), "lastHandshake"), Napi::Date::New(info.Env(), lastHandshake));
       }
 
@@ -75,15 +105,11 @@ Napi::Object getPeers(const Napi::CallbackInfo& info) {
   return DevicesObj;
 }
 
-Napi::Number addDevice(const Napi::CallbackInfo& info) {
-  const char *device_name = info[0].As<Napi::String>().Utf8Value().c_str();
-  return Napi::Number::New(info.Env(), wg_add_device(device_name));
-};
-
 Napi::Number addNewDevice(const Napi::CallbackInfo& info) {
   if (wg_add_device(info[0].As<Napi::String>().Utf8Value().c_str()) < 0) {
     return Napi::Number::New(info.Env(), -1);
   }
+  /*
   char device_name = info[0].As<Napi::String>().Utf8Value().c_str();
   uint16_t portListen = info[1].As<Napi::Number>().Int32Value();
   wg_peer new_peer = {
@@ -99,6 +125,7 @@ Napi::Number addNewDevice(const Napi::CallbackInfo& info) {
   if (wg_set_device(&new_device) < 0) {
     return Napi::Number::New(info.Env(), -2);
   }
+  */
   return Napi::Number::New(info.Env(), 0);
 }
 
@@ -108,7 +135,6 @@ Napi::Number delDevie(const Napi::CallbackInfo& info) {
 };
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set("addDevice", Napi::Function::New(env, addDevice));
   exports.Set("addNewDevice", Napi::Function::New(env, addNewDevice));
   exports.Set("delDevice", Napi::Function::New(env, delDevie));
   exports.Set("getPeers", Napi::Function::New(env, getPeers));
