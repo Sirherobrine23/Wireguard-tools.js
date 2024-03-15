ae17383b5b8e26bc6c3b5367e5cda0534301424b
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2024-03-15 00:47:38 -0300

    #10 Code refactoring

--- addons/tools/linux/set_ip.cpp
+++
@@ -1,6 +0,0 @@
-#include <vector>
-#include <string>
-
-std::string setIps(std::string name, std::vector<std::string> ips) {
-  return "";
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * as key from "./key.js";
-export * as wgQuick from "./quick.js";
+export * as quickConfig from "./quick.js";
 export * from "./wginterface.js";
 export * as wginterface from "./wginterface.js";
-export * as default from "./wginterface.js";
